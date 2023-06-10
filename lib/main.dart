e6dfc154f424abd7865d08d826c980b38c87d086
SahilGurung13 <sahil.gurung.a21.2@icp.edu.np>
2023-06-10 13:13:43 +0545

    day 3 on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // build() method is used to build the widget tree.
     return MaterialApp(
-      // MaterialApp is a widget that implements the basic material design visual layout structure.
-      home: Scaffold(
-        // Scaffold is a widget that provides a framework to implement the basic material design layout of the application.
-        body: Center(
-          // Container is a widget that allows you to customize its child widget.
-          child: Text(
-              "Welcome to Flutter World"), // Text is a widget that allows you to display text.
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to flutter Day1"),
+          ),
         ),
       ),
     );
