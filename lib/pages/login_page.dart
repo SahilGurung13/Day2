2f16c8b7b00d18c75472c1e60a5813ca65e0a333
SahilGurung13 <sahil.gurung.a21.2@icp.edu.np>
2023-06-11 17:26:18 +0545

    day2 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,16 +6,57 @@ class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.redAccent,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Image.asset(
+              "assets/images/logo.png",
+              fit: BoxFit.contain,
+              width: 250,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true, //hide password
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("hi sahil");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
